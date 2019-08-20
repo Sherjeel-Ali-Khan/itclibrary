292c0fdacf7291a7507a9ea9c622148de28d1e8d
jayachanakya <jayachanakya@gmail.com>
2019-08-20 14:50:08 +1000

    Change class name and variable name (Orig: BorrowBookUI UI)
    Change class name and variable name (Orig: BorrowBookUI UI) to BorrowBookUi borrowBookUi
    
    owner of class BorrowBookUi  needs to change class name

--- itclibrary/BorrowBookControl.java
+++ itclibrary/BorrowBookControl.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class BorrowBookControl {
 
-	private BorrowBookUI UI;
+	private BorrowBookUi borrowBookUi;  //Change class name and variable name (Orig: BorrowBookUI UI)
 
 	private Library library; //Change class name and variable name (Orig: library LIBRARY)
 	private member M;
@@ -21,12 +21,12 @@ public class BorrowBookControl {
 	}
 
 
-	public void setUI(BorrowBookUI ui) {
+	public void setUI(BorrowBookUi ui) { // Change class name to BorrowBookUi (Orig: BorrowBookUI)
 		if (!State.equals(CONTROL_STATE.INITIALISED))
 			throw new RuntimeException("BorrowBookControl: cannot call setUI except in INITIALISED state");
 
-		this.UI = ui;
-		ui.Set_State(BorrowBookUI.UI_STATE.READY);
+		this.borrowBookUi = ui; // Change variable name to borrowBookUi (Orig: UI)
+		ui.Set_State(BorrowBookUi.UI_STATE.READY); // Change class name to BorrowBookUi (Orig: BorrowBookUI) // Change class name to BorrowBookUi (Orig: BorrowBookUI)
 		State = CONTROL_STATE.READY;
 	}
 
@@ -37,17 +37,17 @@ public class BorrowBookControl {
 
 		M = library.MEMBER(MEMMER_ID); // Change variable name to library (Orig: LIBRARY)
 		if (M == null) {
-			UI.Display("Invalid memberId");
+			borrowBookUi.Display("Invalid memberId");  // Change variable name to borrowBookUi (Orig: UI)
 			return;
 		}
 		if (library.MEMBER_CAN_BORROW(M)) { // Change variable name to library (Orig: LIBRARY)
 			PENDING = new ArrayList<>();
-			UI.Set_State(BorrowBookUI.UI_STATE.SCANNING);
+			borrowBookUi.Set_State(BorrowBookUi.UI_STATE.SCANNING);  // Change variable name to borrowBookUi (Orig: UI) // Change class name to BorrowBookUi (Orig: BorrowBookUI)
 			State = CONTROL_STATE.SCANNING; }
 		else
 		{
-			UI.Display("Member cannot borrow at this time");
-			UI.Set_State(BorrowBookUI.UI_STATE.RESTRICTED); }}
+			borrowBookUi.Display("Member cannot borrow at this time");  // Change variable name to borrowBookUi (Orig: UI)
+			borrowBookUi.Set_State(BorrowBookUi.UI_STATE.RESTRICTED); }} // Change variable name to borrowBookUi (Orig: UI) // Change class name to BorrowBookUi (Orig: BorrowBookUI)
 
 
 	public void Scanned(int bookId) {
@@ -57,19 +57,19 @@ public class BorrowBookControl {
 		}
 		BOOK = library.Book(bookId); // Change variable name to library (Orig: LIBRARY)
 		if (BOOK == null) {
-			UI.Display("Invalid bookId");
+			borrowBookUi.Display("Invalid bookId");  // Change variable name to borrowBookUi (Orig: UI)
 			return;
 		}
 		if (!BOOK.AVAILABLE()) {
-			UI.Display("Book cannot be borrowed");
+			borrowBookUi.Display("Book cannot be borrowed");  // Change variable name to borrowBookUi (Orig: UI)
 			return;
 		}
 		PENDING.add(BOOK);
 		for (book B : PENDING) {
-			UI.Display(B.toString());
+			borrowBookUi.Display(B.toString());  // Change variable name to borrowBookUi (Orig: UI)
 		}
 		if (library.Loans_Remaining_For_Member(M) - PENDING.size() == 0) { // Change variable name to library (Orig: LIBRARY)
-			UI.Display("Loan limit reached");
+			borrowBookUi.Display("Loan limit reached");  // Change variable name to borrowBookUi (Orig: UI)
 			Complete();
 		}
 	}
@@ -80,12 +80,12 @@ public class BorrowBookControl {
 			cancel();
 		}
 		else {
-			UI.Display("\nFinal Borrowing List");
+			borrowBookUi.Display("\nFinal Borrowing List");   // Change variable name to borrowBookUi (Orig: UI)
 			for (book B : PENDING) {
-				UI.Display(B.toString());
+				borrowBookUi.Display(B.toString());  // Change variable name to borrowBookUi (Orig: UI)
 			}
 			COMPLETED = new ArrayList<loan>();
-			UI.Set_State(BorrowBookUI.UI_STATE.FINALISING);
+			borrowBookUi.Set_State(BorrowBookUi.UI_STATE.FINALISING);  // Change variable name to borrowBookUi (Orig: UI) // Change class name to BorrowBookUi (Orig: BorrowBookUI)
 			State = CONTROL_STATE.FINALISING;
 		}
 	}
@@ -99,17 +99,17 @@ public class BorrowBookControl {
 			loan LOAN = library.ISSUE_LAON(B, M); // Change variable name to library (Orig: LIBRARY)
 			COMPLETED.add(LOAN);
 		}
-		UI.Display("Completed Loan Slip");
+		borrowBookUi.Display("Completed Loan Slip");  // Change variable name to borrowBookUi (Orig: UI)
 		for (loan LOAN : COMPLETED) {
-			UI.Display(LOAN.toString());
+			borrowBookUi.Display(LOAN.toString());  // Change variable name to borrowBookUi (Orig: UI)
 		}
-		UI.Set_State(BorrowBookUI.UI_STATE.COMPLETED);
+		borrowBookUi.Set_State(BorrowBookUi.UI_STATE.COMPLETED);  // Change variable name to borrowBookUi (Orig: UI) // Change class name to BorrowBookUi (Orig: BorrowBookUI)
 		State = CONTROL_STATE.COMPLETED;
 	}
 
 
 	public void cancel() {
-		UI.Set_State(BorrowBookUI.UI_STATE.CANCELLED);
+		UI.Set_State(BorrowBookUi.UI_STATE.CANCELLED);  // Change variable name to borrowBookUi (Orig: UI) // Change class name to BorrowBookUi (Orig: BorrowBookUI)
 		State = CONTROL_STATE.CANCELLED;
 	}
 
