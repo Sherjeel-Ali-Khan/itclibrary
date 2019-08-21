66d75777845bacd7f788e3bb67f0b20ec4e35ea0
Nadeesha Dinu <50892875+Nadeesha1215@users.noreply.github.com>
2019-08-21 18:03:35 +1000

    Update PayFineUI.java
    changed enum names

--- itclibrary/PayFineUI.java
+++ itclibrary/PayFineUI.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 public class PayFineUI {
 
 
-	public static enum UI_STATE { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
+	public static enum UiState { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };// change enum name as UiState
 
 	private PayFineControl control;// change the variable name as control
 	private Scanner input;
-	private UI_STATE state;// change the variable name as state
+	private UiState state;// change the variable name as state, enum name as UiState
 
 	
 	public PayFineUI(PayFineControl control1) {//change parameter name control to control1 for easyli identify with variable name
 		this.control = control1;// change according to parameter and variable name control1 and control
 		input = new Scanner(System.in);
-		state = UI_STATE.INITIALISED;// change according to variable name state
+		state = UiState.INITIALISED;// change according to variable name state , enum name as UiState
 		control1.setUi(this);// change according to parameter name control1 and method name setUi
 	}
 	
-	//change the mathod name as setState
-	public void setState(UI_STATE state1) {//change parameter name state to state1 for easyli identify with variable name
+	//change the mathod name as setState, enum name as UiState
+	public void setState(UiState state1) {//change parameter name state to state1 for easyli identify with variable name
 		this.state = state1;// change according to parameter and variable name state1 and state
 	}
 
