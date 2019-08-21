2588ea006c362cc912cf8a086b6810aa9c12f450
Nadeesha Dinu <50892875+Nadeesha1215@users.noreply.github.com>
2019-08-21 19:11:58 +1000

    Update PayFineUI.java
    final checking

--- itclibrary/PayFineUI.java
+++ itclibrary/PayFineUI.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class PayFineUI {
-
+    
 	public static enum UiState { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };// change enum name as UiState
 
 	private PayFineControl control;// change the variable name as control
 	private Scanner input;
 	private UiState state;// change the variable name as state, enum name as UiState
 
+	
 	public PayFineUI(PayFineControl control1) {//change parameter name control to control1 for easyli identify with variable name
 		this.control = control1;// change according to parameter and variable name control1 and control
 		input = new Scanner(System.in);
@@ -20,6 +21,7 @@ public class PayFineUI {
 		this.state = state1;// change according to parameter and variable name state1 and state
 	}
 
+
 	public void isRun() {// change method name as isRun
 		setOutput("Pay Fine Use Case UI\n");// change using method name as setOutput
 		
@@ -28,44 +30,44 @@ public class PayFineUI {
 			switch (state) {// change the variable name as state
 			
 				case READY:
-					String Mem_Str = setInput("Swipe member card (press <enter> to cancel): ");// change using method name as setInput
-					if (Mem_Str.length() == 0) {
+					String memberString = setInput("Swipe member card (press <enter> to cancel): ");// change using method name as setInput, variable name memberString
+					if (memberString.length() == 0) {//change variable name as memberString
 						control.isCancel();// change according to variable name control and method name isCancel
 						break;
 					}
 					try {
-						int Member_ID = Integer.valueOf(Mem_Str).intValue();
-						control.cardSwiped(Member_ID);// change according to variable name control and method name cardSwiped
+						int memberId = Integer.valueOf(memberString).intValue();//change variable name as memberString, memberId
+						control.cardSwiped(memberId);// change according to variable name control, memberId and method name cardSwiped
 					} catch (NumberFormatException e) {
-					setOutput("Invalid memberId");// change using method name as setOutput
+						setOutput("Invalid memberId");// change using method name as setOutput
 					}
 					break;
-				
+					
 				case PAYING:
-					double AmouNT = 0;
-					String Amt_Str = setInput("Enter amount (<Enter> cancels) : ");// change using method name as setInput
-					if (Amt_Str.length() == 0) {
+					double amount = 0;// change variable name as amount
+					String amountString = setInput("Enter amount (<Enter> cancels) : ");// change using method name as setInput, variable name as amountString
+					if (amountString.length() == 0) {// change variable name as amountString
 						control.isCancel();// change according to variable name control and method name isCancel
 						break;
 					}
 					try {
-						AmouNT = Double.valueOf(Amt_Str).doubleValue();
+						amount = Double.valueOf(amountString).doubleValue();// change variable name as amount,amountString
 					} catch (NumberFormatException e) {}
-					if (AmouNT <= 0) {
+					if (amount <= 0) {// change variable name as amount
 						setOutput("Amount must be positive");// change using method name as setOutput
 						break;
 					}
-					control.payFine(AmouNT);// change according to variable name control and method name payFine
+					control.payFine(amount);// change according to variable name control, amount and method name payFine
 					break;
-								
+									
 				case CANCELLED:
 					setOutput("Pay Fine process cancelled");// change using method name as setOutput
 					return;
-			
+				
 				case COMPLETED:
 					setOutput("Pay Fine process complete");// change using method name as setOutput
 					return;
-			
+				
 				default:
 					setOutput("Unhandled state");// change using method name as setOutput
 					throw new RuntimeException("FixBookUI : unhandled state :" + state);// change the variable name as state			
@@ -73,18 +75,22 @@ public class PayFineUI {
 			}		
 		}		
 	}
+
 	
 	private String setInput(String prompt) {// change method name as setInput
 		System.out.print(prompt);
 		return input.nextLine();
-	}		
+	}	
+		
 		
 	private void setOutput(Object object) {// change method name as setOutput
 		System.out.println(object);
 	}	
 			
+
 	public void isDisplay(Object object) {// change method name as display
 		setOutput(object);// change using method name as setOutput
 	}
+
 
 }
