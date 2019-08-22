6d053d9f657d97cb693c876260c277c49da652b8
jayachanakya <jayachanakya@gmail.com>
2019-08-23 09:54:26 +1000

    Update BorrowBookUI.java
    Issues fixed which are raised in review proccess

--- itclibrary/BorrowBookUI.java
+++ itclibrary/BorrowBookUI.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class BorrowBookUI { // Class name changed ( To: BorrowBookUi ; Orig: BorrowBookUI;)
+public class BorrowBookUI { // Class name changed ( To: BorrowBookUI ; Orig: BorrowBookUI;)
 
 	public static enum UiState { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };  //Change enum name ( To: enum UiState; Orig: enum UI_STATE;)
 
@@ -10,11 +10,11 @@ public class BorrowBookUI { // Class name changed ( To: BorrowBookUi ; Orig: Bor
 	private UiState uiState; //Change enu name and variable name ( To: UiState uiState; Orig: UI_STATE StaTe;)
 
 
-	public BorrowBookUI(BorrowBookControl control) { // Default constructor name changed ( To: BorrowBookUi ; Orig: BorrowBookUI;)
+	public BorrowBookUI(BorrowBookControl control) { // Default constructor name changed ( To: BorrowBookUI ; Orig: BorrowBookUI;)
 		this.browBkCntrl = control; // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
 		input = new Scanner(System.in);
 		uiState = UiState.INITIALISED; //Change enu name and variable name ( To: uiState = UiState.INITIALISED; Orig: StaTe = UI_STATE.INITIALISED;)
-		control.setUI(this);
+		control.setUi(this); //change method name (To: setUi Orig: setUI)
 	}
 
 
@@ -29,7 +29,7 @@ public class BorrowBookUI { // Class name changed ( To: BorrowBookUi ; Orig: Bor
 	}
 
 
-	public void Set_State(UiState state) { // Change parameter name ( To: state;  Orig: STATE;)
+	public void setState(uiState state) { // Change parameter name ( To: state;  Orig: STATE;) Change method name (To:setState; Orig:Set_State;)
 		this.uiState = state; // Change local variable name ( To: this.uiState = state;  Orig: UI_STATE = STATE; )
 	}
 
@@ -53,8 +53,8 @@ public class BorrowBookUI { // Class name changed ( To: BorrowBookUi ; Orig: Bor
 					break;
 				}
 				try {
-					int Member_ID = Integer.valueOf(MEM_STR).intValue();
-					browBkCntrl.swiped(Member_ID); // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
+					int memberId = Integer.valueOf(MEM_STR).intValue(); // Change variable name ( To: memberId; Orig: Member_ID;)
+					browBkCntrl.Swiped(memberId); // Change variable name ( To: browBkCntrl; Orig: CONTROL;) ( To: memberId; Orig: Member_ID;)
 				}
 				catch (NumberFormatException e) {
 					output("Invalid Member Id");
@@ -71,12 +71,12 @@ public class BorrowBookUI { // Class name changed ( To: BorrowBookUi ; Orig: Bor
 			case SCANNING:
 				String Book_Str = input("Scan Book (<enter> completes): ");
 				if (Book_Str.length() == 0) {
-					browBkCntrl.complete(); // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
+					browBkCntrl.Complete(); // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
 					break;
 				}
 				try {
-					int BiD = Integer.valueOf(Book_Str).intValue();
-					browBkCntrl.scanned(BiD); // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
+					int bid = Integer.valueOf(Book_Str).intValue(); // Change variable name ( To: bid; Orig: BiD;)
+					browBkCntrl.Scanned(bid); // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
 
 				} catch (NumberFormatException e) {
 					output("Invalid Book Id");
@@ -85,12 +85,12 @@ public class BorrowBookUI { // Class name changed ( To: BorrowBookUi ; Orig: Bor
 
 
 			case FINALISING:
-				String Ans = input("Commit loans? (Y/N): ");
-				if (Ans.toUpperCase().equals("N")) {
+				String userAnswer = input("Commit loans? (Y/N): "); // Change variable name ( To: userAnswer; Orig: Ans;)
+				if (userAnswer.toUpperCase().equals("N")) {
 					browBkCntrl.cancel(); // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
 
 				} else {
-					browBkCntrl.commitLoans(); // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
+					browBkCntrl.Commit_LOans(); // Change variable name ( To: browBkCntrl; Orig: CONTROL;)
 					input("Press <any key> to complete ");
 				}
 				break;
@@ -109,7 +109,7 @@ public class BorrowBookUI { // Class name changed ( To: BorrowBookUi ; Orig: Bor
 	}
 
 
-	public void Display(Object object) {
+	public void setDisplay(Object object) {  //change method name (To: setDisplay Orig: Display)
 		output(object);
 	}
 
